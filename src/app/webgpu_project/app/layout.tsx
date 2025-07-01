dab5546f17dfb2a3c3d3157cf7e319d757be4842
ConeF <conef@qq.com>
2025-07-01 20:09:16 +0800

    移除主题提供者组件，简化根布局

--- src/app/webgpu_project/app/layout.tsx
+++ src/app/webgpu_project/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./contexts/ThemeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
